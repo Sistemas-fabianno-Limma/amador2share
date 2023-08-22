7c82de0fe661aa8caaa714af6485fb25ca968e4a
Fabianno Limma <fabiannolimma20287@gmail.com>
2023-08-22 20:00:54 -0300

    configurando uma melhor experiencia no login

--- setting/login.php
+++ setting/login.php
@@ -5,8 +5,42 @@ $senha = inputCheck($_POST['senha']);
 $checkPlayer ="SELECT * FROM players WHERE Usuario='". $usuario ."'";
 $resultPlayer = $con->query($checkPlayer);
 while($row = $resultPlayer->fetch_assoc()) {
-echo $userData = $row['Usuario'];
-echo $passData = $row['PassWord'];
+$userData = $row['Usuario'];
+$passData = $row['PassWord'];
   }
+ if(empty($usuario) || empty($senha)) {
+    echo "
+    <div class='line'></div>
+    <h1><span>Ops!</span> Você não pode deixar campos em branco!</h1>
+    <a class='button' href='?page=login'>Voltar a Página de login etente novamente</a>
+    <div class='line'></div>
+    ";   
+ }else{
+if($resultPlayer -> num_rows() > 0){
+if($senha == $passData){
+//////////////////////////////////////
+    echo "
+    <div class='line'></div>
+    <h1><span>Nice!</span>Login Liberado!</h1>
+    <div class='line'></div>
+    ";  
+    /////////////////////////////////
+}else{   
+        echo "
+        <div class='line'></div>
+        <h1><span>Ops!</span> Essa senha não confere!</h1>
+        <a class='button' href='?page=login'>Voltar a Página de login etente novamente</a>
+        <div class='line'></div>
+       "; 
+}
+}else{
+       echo "
+       <div class='line'></div>
+      <h1><span>Ops!</span> Esse usuário não confere!</h1>
+      <a class='button' href='?page=login'>Voltar a Página de login etente novamente</a>
+      <div class='line'></div>
+      ";    
+}
+}
 
 ?>
